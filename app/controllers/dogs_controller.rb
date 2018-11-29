631355fcba4465fbd1da1ec87c905f40e6f20151
paristuzun <paristuzun@gmail.com>
2018-11-29 13:14:23 -0500

    Fixed minor issues

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -2,8 +2,9 @@ class DogsController < ApplicationController
 
   def index
     @dogs = Dog.all
-      @dogs = @dogs.sort_by {|dog| dog.employees.length }
-    
+      # @dogs = @dogs.sort_by {|dog| dog.employees.length }
+       # @dogs = Dog.sort_by_highest_num_owners
+
   end
 
   def show
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,12 @@
 class Dog < ApplicationRecord
   has_many :employees
+
+  # def employee_count
+  #   self.employees.length
+  # end
+  #
+  # def self.sort_by_highest_num_owners
+  #   Dog.all.sort_by{|d| d.highest_num_owners}
+  # end
+
 end
